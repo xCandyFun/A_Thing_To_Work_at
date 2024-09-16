5c3c16cd217147c8a724bafcf7774226134ea239
Simon T. <simtho0624@gmail.com>
2024-09-16 13:38:06 +0200

    Fix the Add Task

--- src/main/java/org/example/To_Do_List/Main.java
+++ src/main/java/org/example/To_Do_List/Main.java
@@ -1,7 +1,49 @@
 package org.example.To_Do_List;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    static Integer Id = 0;
+
     public static void main(String[] args) {
 
+        while (true) {
+            menu();
+            Integer pick = sc.nextInt();
+            switch (pick) {
+                case (1) -> System.exit(1);
+                case (2) -> AddTask();
+            }
+        }
+
+    }
+
+
+    public static void AddTask() {
+
+        System.out.println("Add a new task to the list: ");
+        String newTask = sc.next();
+
+        Id++;
+
+        String fullTask = Id + " " + newTask;
+
+        System.out.println(fullTask);
+
+    }
+
+    public void RemoveTask() {
+    }
+
+    public void MarkTask() {
+    }
+
+    public static void menu() {
+        System.out.println("""
+                1: Exit the program
+                2: Add Task""");
     }
 }
